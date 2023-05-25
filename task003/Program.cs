0078aab8872361098f8c6bbab77aea54d121bc37
RubansGo <a.moran2017@yandex.ru>
2023-05-25 14:14:03 +0300

    Третья задача

---
+++ task003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a day of week");
+int Day = Convert.ToInt32(Console.ReadLine());
+if (Day >= 1 && Day <=7) {
+	if (Day >= 6) {
+		Console.Write("It's a holiday");
+	} else {
+		Console.Write("Working day");
+	}
+} else {
+	Console.Write("Enter a valid day between 1 and 7");
+}
